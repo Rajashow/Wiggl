8a8d40f5bb447461b7b93d0f2e781096389249cd
amandotzip <aawoke@terpmail.umd.edu>
2020-11-14 02:58:41 -0500

    comments

--- server.js
+++ server.js
@@ -2,19 +2,27 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const hostname = '127.0.0.1'
+const port = 3000
 const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+/** returns website title?
+redirects to root websit?
+gets request to uuid? is it for random rooms for users?*/
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
+/** Redirects to different rooms as well? */
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
+
+/** socket.io IO */
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
@@ -26,4 +34,8 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+/** choose a port */
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+
