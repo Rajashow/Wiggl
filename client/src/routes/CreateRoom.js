44dc04ec16beed39e9962125fde1bfce357fb1fe
Rajashow <rajashowparajuli@gmail.com>
2020-11-15 09:23:57 -0500

    updated css

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -2,14 +2,18 @@ import React from "react";
 import { v1 as uuid } from "uuid";
 
 const CreateRoom = (props) => {
-    function create() {
-        const id = uuid();
-        props.history.push(`/room/${id}`);
-    }
+  function create() {
+    const id = uuid();
+    props.history.push(`/room/${id}`);
+  }
 
-    return (
+  return (
+    <div class="container">
+      <div class="vertical-center">
         <button onClick={create}>Create Room</button>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default CreateRoom;
